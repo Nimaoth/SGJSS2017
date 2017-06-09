8711a62607d7adbe8cbe15d25c5785e93b4e1237
er <ga87jun@in.tum.de>
2017-06-09 22:34:20 +0200

    Playermovement Beginning

---
+++ SGJSS2017/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float playerSpeed;
+    public static int lives = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //player Movement
+        #region PlayerMovement
+        //Move player based on user input
+        float amtToMove = Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime;
+        float amtToMoveUp = Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime;
+
+
+
+        #endregion
+    }
+}
