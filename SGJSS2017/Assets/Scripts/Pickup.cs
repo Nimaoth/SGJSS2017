adbf66a301d9a4597b713772e8908f241fea4187
er <ga87jun@in.tum.de>
2017-06-10 14:45:56 +0200

    no message

---
+++ SGJSS2017/Assets/Scripts/Pickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    private Material material;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Game.Instance.Speed * Time.deltaTime;
+        transform.Translate(0, offset, 0);
+
+        // destroy if out of screen
+        if (transform.position.y > 50)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player1" || other.tag == "player2")
+        {
+            Buff(other.transform.parent);
+        }
+    }
+
+    Buff()
+    {
+
+    }
+}
